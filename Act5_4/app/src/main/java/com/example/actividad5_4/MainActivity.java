d4b039a4f8f5da201848985dbd2836a6c44196ce
Pabl0R0driguez <pablorodseg@gmail.com>
2024-10-21 18:32:01 +0200

    Página 102 ejercicio 5_4

--- Act5_4/app/src/main/java/com/example/actividad5_4/MainActivity.java
+++ Act5_4/app/src/main/java/com/example/actividad5_4/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.actividad5_4;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,12 +18,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button boton1 = findViewById(R.id.boton1);
+        Button boton2 = findViewById(R.id.boton2);
+        Button boton3 = findViewById(R.id.boton3);
+        Button boton4 = findViewById(R.id.boton4);
+
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Teclado1.class));
+            }
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Teclado2.class));
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Teclado3.class));
+            }
+        });
+
+        boton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Teclado4.class));
+            }
         });
     }
+
+
 }
