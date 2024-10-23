5c922d696f6b36a52a3d7d6e86d8fc5181f7954a
Pabl0R0driguez <pablorodseg@gmail.com>
2024-10-23 20:06:10 +0200

    d

---
+++ Ejercicio3.5/P65E3_5/app/src/main/java/android/example/p65_e3_5/MainActivity.java
@@ -0,0 +1,29 @@
+package android.example.p65_e3_5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+            Intent ejemplo = new Intent(this, MainActivity2.class);
+            startActivity(ejemplo);
+
+    }
+}
---
+++ Ejercicio4.1/P79E4_1/app/src/main/java/android/example/p79_e4_1/MainActivity.java
@@ -0,0 +1,27 @@
+package android.example.p79_e4_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Intent borrar = new Intent(this, MainActivity2.class);
+        startActivity(borrar);
+    }
+}
